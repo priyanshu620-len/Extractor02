de63068203ccbc0f6f009e0f4a272ae77c2b7130
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-23 23:04:33 +0530

    Update API credentials and channel IDs in config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "c1f27c622acecf9bf6e71d0d295e75f9")
-BOT_TOKEN = getenv("BOT_TOKEN", "8172684751:AAFrKOePs4i_8Zbo3OciESAnP40EVT9cQlQ")
-OWNER_ID = int(getenv("OWNER_ID", "8152589217"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8152589217,6073936818").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "34320281"))
+API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
+BOT_TOKEN = getenv("BOT_TOKEN", "8372745246:AAFsqcrX5a8R6dEuPhgvChBvU-EDSzRjddg")
+OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8301160173,8562989428").split()))
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://ONeX_db_user:onexvartikuu142062@cluster0.ga3zort.mongodb.net/?appName=Cluster0")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002656120306"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002630993737"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003533149811"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003533149811"))
 
 """
 #
