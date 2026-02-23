cf90e5422ca19d51c0c2882fef0d2fdc52c4c3b1
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-23 23:16:58 +0530

    Update SUDO_USERS environment variable values

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = int(getenv("API_ID", "34320281"))
 API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
 BOT_TOKEN = getenv("BOT_TOKEN", "8372745246:AAFsqcrX5a8R6dEuPhgvChBvU-EDSzRjddg")
 OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8301160173,8562989428").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8152589217,6073936818").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://ONeX_db_user:onexvartikuu142062@cluster0.ga3zort.mongodb.net/?appName=Cluster0")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003533149811"))
@@ -34,7 +34,7 @@ BOT_USERNAME = os.environ.get("extractor011_bot")
 OWNER_ID = int(os.environ.get("OWNER_ID", "8301160173"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8301160173,8562989428").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6073936818,8152589217").split()))
 # ------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003533149811"))
 # ------------------------------------------------
