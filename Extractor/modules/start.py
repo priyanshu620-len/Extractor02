610fdf42c4a8e4d40d9bc0b266d52fe05cdcbd31
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-24 18:18:19 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -46,7 +46,7 @@ MAIN_BUTTONS = InlineKeyboardMarkup([
     ],
     [
         InlineKeyboardButton("📋 TXT → HTML", callback_data="txt_html"),
-        InlineKeyboardButton("👨‍💻 Developer", url=f"tg://user?id={OWNER_ID}")
+        InlineKeyboardButton("👨‍💻 Developer", url=f"tg://user?id={8301160173}")
     ],
     [InlineKeyboardButton("📄 Get OMR", callback_data="get_omr")],
     [InlineKeyboardButton("❌ Close Menu", callback_data="home_")]
