e547acfad31a8597dac6f181e3c41f7a1768f686
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-26 16:51:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "34320281"))
 API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
-BOT_TOKEN = getenv("BOT_TOKEN", "8293526606:AAGjdCsTSg8FEHnViDz-mDycltk4atcq-EE")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8152589217,6073936818").split(",")))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://ONeX_db_user:onexvartikuu142062@cluster0.ga3zort.mongodb.net/?appName=Cluster0")
@@ -26,7 +26,7 @@ from os import getenv
 # Basic Bot Configs
 API_ID = int(getenv("API_ID", "34320281"))
 API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
-BOT_TOKEN = getenv("BOT_TOKEN", "8293526606:AAGjdCsTSg8FEHnViDz-mDycltk4atcq-EE")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # IDs & Database
 OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
