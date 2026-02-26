ad62ca8463f0e7bf1f1bdd306889eef37c1c2790
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-26 16:47:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,23 +22,27 @@ PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003533149811"))
 
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "34320281"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8293526606:AAGjdCsTSg8FEHnViDz-mDycltk4atcq-EE")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("extractor011_bot")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "8301160173"))
-# ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6073936818,8152589217").split(",")))
-# ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003533149811"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://ONeX_db_user:onexvartikuu142062@cluster0.ga3zort.mongodb.net/?appName=Cluster0")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003533149811"))
+# Basic Bot Configs
+API_ID = int(getenv("API_ID", "34320281"))
+API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
+BOT_TOKEN = getenv("BOT_TOKEN", "8293526606:AAGjdCsTSg8FEHnViDz-mDycltk4atcq-EE")
 
+# IDs & Database
+OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8152589217,6073936818").split(",")))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ONeX_db_user:onexvartikuu142062@cluster0.ga3zort.mongodb.net/?appName=Cluster0")
+
+# Logs & Channels
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003533149811"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003533149811"))
+
+# --- YEH VARIABLE MISSING THA ---
+# Aapka Telegram Channel Link jahan report mein 'Join Us' dikhega
+join = getenv("JOIN_URL", "https://t.me/your_channel_username") 
+
+# Bot Username
+BOT_USERNAME = getenv("BOT_USERNAME", "extractor011_bot")
+
+# Thumbnail URL (Optional)
+THUMB_URL = getenv("THUMB_URL", "https://telegra.ph/file/your_thumb_id.jpg")
