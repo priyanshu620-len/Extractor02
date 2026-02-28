079f077dc7e6044501ee934daf162a094e386e0e
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-02-28 18:45:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "34320281"))
 API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
-BOT_TOKEN = getenv("BOT_TOKEN", "8704380201:AAH_acEogKdVxY8p6DoQ_UUzaYRBMXaZdCw")
+BOT_TOKEN = getenv("BOT_TOKEN", "8716083436:AAEkDqnXsDlNuMHhKEJ24zO_vima4b9XLi4")
 OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8152589217,6073936818").split(",")))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://ONeX_db_user:onexvartikuu142062@cluster0.ga3zort.mongodb.net/?appName=Cluster0")
@@ -26,7 +26,7 @@ from os import getenv
 # Basic Bot Configs
 API_ID = int(getenv("API_ID", "34320281"))
 API_HASH = getenv("API_HASH", "e55840d9b1ba82ba7748a30e2bbbfc1a")
-BOT_TOKEN = getenv("BOT_TOKEN", "8704380201:AAH_acEogKdVxY8p6DoQ_UUzaYRBMXaZdCw")
+BOT_TOKEN = getenv("BOT_TOKEN", "8716083436:AAEkDqnXsDlNuMHhKEJ24zO_vima4b9XLi4")
 
 # IDs & Database
 OWNER_ID = int(getenv("OWNER_ID", "8301160173"))
@@ -42,7 +42,7 @@ PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1003533149811"))
 join = getenv("JOIN_URL", "https://t.me/+u9LKPzpFRgtkN2E9") 
 
 # Bot Username
-BOT_USERNAME = getenv("BOT_USERNAME", "@Extractor04_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "@ONeXCodebot")
 
 # Thumbnail URL (Optional)
 THUMB_URL = getenv("THUMB_URL", "https://telegra.ph/file/your_thumb_id.jpg")
